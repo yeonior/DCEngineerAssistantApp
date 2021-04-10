78cfdf9b4b3a4d371a481e25d9e8e77fe3a4d1e0
Rus <de.mr44@gmail.com>
2021-04-10 15:32:26 +0500

    calculator page is done

--- DCEngineerAssistant/CalculatorVC.swift
+++ DCEngineerAssistant/CalculatorVC.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class CalculatorVC: UIViewController {
     
-    var firstOktetOfAddress, secondOktetOfAddress, thirdOktetOfAddress, fourthOktetOfAddress: UInt8?
+    
+    var firstOktetOfNet, secondOktetOfNet, thirdOktetOfNet, fourthOktetOfNet: UInt8?
     var firstOktetOfMask, secondOktetOfMask, thirdOktetOfMask, fourthOktetOfMask: UInt8?
+    var firstOktetOfAddress, secondOktetOfAddress, thirdOktetOfAddress, fourthOktetOfAddress: UInt8?
+    
     var masksDictionary = [
+        0: "0.0.0.0",
+        1: "128.0.0.0",
+        2: "192.0.0.0",
+        3: "224.0.0.0",
+        4: "240.0.0.0",
+        5: "248.0.0.0",
+        6: "252.0.0.0",
+        7: "254.0.0.0",
+        8: "255.0.0.0",
+        9: "255.128.0.0",
+        10: "255.192.0.0",
+        11: "255.224.0.0",
+        12: "255.240.0.0",
+        13: "255.248.0.0",
+        14: "255.252.0.0",
+        15: "255.254.0.0",
         16: "255.255.0.0",
         17: "255.255.128.0",
         18: "255.255.192.0",
@@ -26,50 +45,47 @@ class CalculatorVC: UIViewController {
         27: "255.255.255.224",
         28: "255.255.255.240",
         29: "255.255.255.248",
-        30: "255.255.255.252"
+        30: "255.255.255.252",
+        31: "255.255.255.254",
+        32: "255.255.255.255"
     ]
     
     @IBOutlet weak var net: UITextField!
     @IBOutlet weak var broadcast: UITextField!
-    @IBOutlet weak var fullMask: UITextField!
-    @IBOutlet weak var address: UITextField!
     @IBOutlet weak var mask: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var prefix: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         address.delegate = self
-        mask.delegate = self
-
+        prefix.delegate = self
+        
+        net.text = "N/A"
+        broadcast.text = "N/A"
+        mask.text = "N/A"
     }
     
     // Скрываем клавиатуры для любых объектов
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
 
 extension  CalculatorVC: UITextFieldDelegate {
     
+    // Проверка октетов адреса
     func verifyOctet(test: String) -> Bool {
         let pattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])[.]){0,3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])?$"
         let regexText = NSPredicate(format: "SELF MATCHES %@", pattern)
         let result = regexText.evaluate(with: test)
         return result
     }
-
+    
+    // Проверка адреса целиком
     func verifyIP(test: String) -> Bool {
         let pattern = "(25[0-5]|2[0-4]\\d|1\\d{2}|\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|\\d)"
         let regexText = NSPredicate(format: "SELF MATCHES %@", pattern)
@@ -77,6 +93,7 @@ extension  CalculatorVC: UITextFieldDelegate {
         return result
     }
     
+    // Проверка маски
     func verifyMask(test: String) -> Bool {
         let pattern = "([0-9]|1[0-9]|2[0-9]|3[0-2]|25[0-5])"
         let regexText = NSPredicate(format: "SELF MATCHES %@", pattern)
@@ -84,68 +101,118 @@ extension  CalculatorVC: UITextFieldDelegate {
         return result
     }
     
+    // Расчет адреса сети
     func calculationNet() {
-        guard let a = firstOktetOfAddress, let b = firstOktetOfMask, let c = secondOktetOfAddress, let d = secondOktetOfMask, let e = thirdOktetOfAddress, let f = thirdOktetOfMask, let g = fourthOktetOfAddress, let h = fourthOktetOfMask else { return }
+        guard let a = firstOktetOfAddress,
+              let b = firstOktetOfMask,
+              let c = secondOktetOfAddress,
+              let d = secondOktetOfMask,
+              let e = thirdOktetOfAddress,
+              let f = thirdOktetOfMask,
+              let g = fourthOktetOfAddress,
+              let h = fourthOktetOfMask else { return }
+        
+        firstOktetOfNet = a & b
+        secondOktetOfNet = c & d
+        thirdOktetOfNet = e & f
+        fourthOktetOfNet = g & h
+        
         net.text = "\(a & b).\(c & d).\(e & f).\(g & h)"
     }
     
-    // Проводим расчет перед началом редактирования
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        SetCorrectDecimalKeyboard(UITextField: textField)
+    // Расчет широковещательного адреса
+    func calculationBroadcast() {
+        guard let a = firstOktetOfNet,
+              let b = firstOktetOfMask,
+              let c = secondOktetOfNet,
+              let d = secondOktetOfMask,
+              let e = thirdOktetOfNet,
+              let f = thirdOktetOfMask,
+              let g = fourthOktetOfNet,
+              let h = fourthOktetOfMask else { return }
+        
+        broadcast.text = "\(a | ~b).\(c | ~d).\(e | ~f).\(g | ~h)"
     }
     
-    // Проводим расчет во время редактирования
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-
-    }
-    
-    // Проводим расчет после редактирования
-    func textFieldDidEndEditing(_ textField: UITextField) {
+    // Общий расчет
+    func textFieldCheckAndCalculation(_ textField: UITextField) {
         
         switch textField {
         
         case address:
+            
             let buffer = address.text!
             let array = buffer.components(separatedBy: ".")
-            if array.count == 4 {
+            
+            if array.count == 4 && array[3] != "" {
                 firstOktetOfAddress = UInt8(array[0])
                 secondOktetOfAddress = UInt8(array[1])
                 thirdOktetOfAddress = UInt8(array[2])
                 fourthOktetOfAddress = UInt8(array[3])
                 
                 calculationNet()
+                calculationBroadcast()
+            } else {
+                net.text = "N/A"
+                broadcast.text = "N/A"
+                firstOktetOfNet = nil; secondOktetOfNet = nil; thirdOktetOfNet = nil; fourthOktetOfNet = nil
+                firstOktetOfAddress = nil; secondOktetOfAddress = nil; thirdOktetOfAddress = nil; fourthOktetOfAddress = nil
             }
             
-        case mask:
+        case prefix:
             
-            let buffer = mask.text!
+            let buffer = prefix.text!
             let x = Int(buffer)
+            
             if x != nil {
-                let mask = masksDictionary[x!]
-                fullMask.text = String(mask!)
-                let array = mask!.components(separatedBy: ".")
+                let currentMask = masksDictionary[x!]
+                mask.text = String(currentMask!)
+                let array = currentMask!.components(separatedBy: ".")
                 firstOktetOfMask = UInt8(array[0])
                 secondOktetOfMask = UInt8(array[1])
                 thirdOktetOfMask = UInt8(array[2])
                 fourthOktetOfMask =  UInt8(array[3])
                 
                 calculationNet()
+                calculationBroadcast()
+            } else {
+                net.text = "N/A"
+                broadcast.text = "N/A"
+                mask.text = "N/A"
+                firstOktetOfMask = nil; secondOktetOfMask = nil; thirdOktetOfMask = nil; fourthOktetOfMask = nil
             }
             
-        default:
-            return
+        default: return
         }
     }
     
+    // Перед редактированием
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == address {
+            SetCorrectDecimalKeyboard(UITextField: textField)
+        }
+    }
+    
+    // Во время редактирования
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        textFieldCheckAndCalculation(textField)
+    }
+    
+    // После редактирования
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textFieldCheckAndCalculation(textField)
+    }
+    
+    // Проверка вводимой информации
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
+        
         switch textField {
         case address:
             let addressText = address.text ?? ""
             let addressState = verifyOctet(test: addressText + string) || verifyIP(test: addressText + string)
             return addressState
-        case mask:
-            let maskText = mask.text ?? ""
+        case prefix:
+            let maskText = prefix.text ?? ""
             let maskState = verifyMask(test: maskText + string)
             return maskState
         default:
@@ -153,27 +220,21 @@ extension  CalculatorVC: UITextFieldDelegate {
         }
     }
     
+    // Показ клавиатуры в зависимости от языка
     func SetCorrectDecimalKeyboard(UITextField: UITextField)  {
-       
+        
         let language = Locale.current.identifier
-       
+        
         if (language == "en_GB" || language == "en_US") {
-           
-            print("[LOG] \(language) - Setting Decimal keyboard")
             UITextField.keyboardType = UIKeyboardType.decimalPad
-        }
-        else {
-           
-            print("[LOG] \(language) - Setting Numbers & Punctuation keyboard")
+        } else {
             UITextField.keyboardType = UIKeyboardType.numbersAndPunctuation
         }
     }
     
-//    //When Enter is tapped
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        return true
-//    }
-    
-
+    // Скрываем клавиатуру по нажатию на Ввод
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
